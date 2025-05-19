3acc105cbf2bebfb10f66e9e5622fa8f05c3370a
anaol216 <atinafuanaol@gmail.com>
2025-05-20 01:30:29 +0300

    build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+
+# Install dependencies
 pip install -r requirements.txt
+pip install kaggle
+
+# Setup Kaggle API key
+mkdir -p ~/.kaggle
+echo "$KAGGLE_JSON" > ~/.kaggle/kaggle.json
+chmod 600 ~/.kaggle/kaggle.json
+
+# Download dataset
+kaggle datasets download -d arashnic/an-unbiased-sequential-recommendation-dataset
+
+# Unzip the dataset
+unzip an-unbiased-sequential-recommendation-dataset.zip -d data
+
+# Run your notebook script
 python main.py
